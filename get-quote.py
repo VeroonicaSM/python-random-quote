9addf76c87fd512e6fe796905c554c9478f8cead
VeroonicaSM <81553224+VeroonicaSM@users.noreply.github.com>
2021-03-31 19:54:18 +1300

    Random quote is working

--- get-quote.py
+++ get-quote.py
@@ -1,8 +1,13 @@
+import random
+
 def answer():
-    print("Keep it logically awesome.")
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-  #print(quotes)
+    
+    #print("Keep it logically awesome.")
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  last=13
+  rdn=random.randint(0,last)
+  print(quotes[rdn])
 if __name__== "__main__":
     answer()
